a2118400176c978feae00cf2cc3d930aeb8f7d75
alu0100601398 <alu0100601398@ull.edu.es>
2015-12-22 13:18:45 +0000

    Added rewards, splitted by rank

--- bot.py
+++ bot.py
@@ -33,6 +33,27 @@ def listener(messages):
 bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
 #############################################
 #Funciones
+def split_len(seq, length):
+    return [seq[i:i+length] for i in range(0, len(seq), length)]
+
+def format_string(string):
+    a = string.split('\n')
+    b = []
+    result = ''
+    print a
+    for i in a:
+        b.append(i.split(' ')[0])
+    bigger=''
+    for j in b:
+        if len(j) > len(bigger):
+            bigger = j
+    for i in range(len(bigger)):
+        result +='_'
+        
+    result += a[0]
+    
+    return result
+    
 @bot.message_handler(commands=['debilidades']) # Indicamos que lo siguiente va a controlar el comando '/roto2'.
 def command_debilidades(m): # Definimos una función que resuelva lo que necesitemos.
     url = "http://kiranico.com/es/mh4u/monstruo/"
@@ -80,47 +101,108 @@ def command_debilidades(m): # Definimos una función que resuelva lo que necesit
             if i == 5:
                 #print entradas
                 values = re.findall(r'var.*?=\s*(.*?);', str(entradas), re.DOTALL | re.MULTILINE)
-                j=0
-                for x in str(values).split(","):
-                    if x.find("res_fire") != -1 and j < 20:
-                        fire += x.split(':')[1].split('\"')[1]
-                        fire += " "
-                        j+=1
-                    if x.find("res_thunder") != -1 and j < 20:
-                        thunder += x.split(':')[1].split('\"')[1]
-                        thunder += " "
-                        j+=1
-                    if x.find("res_water") != -1 and j < 20:
-                        water += x.split(':')[1].split('\"')[1]
-                        water += " "
-                        j+=1
-                    if x.find("res_ice") != -1 and j < 20:
-                        ice += x.split(':')[1].split('\"')[1]
-                        ice += " "
-                        j+=1
-                    if x.find("res_dragon") != -1 and j < 20:
-                        dragon += x.split(':')[1].split('\"')[1]
-                        dragon += " "
-                        j+=1
-                
-        hp = "HP " + str(values).split(',')[1].split(':')[1]
-        print hp
-        print fire
-        print thunder
-        print water
-        print ice
-        print dragon
-        result = hp + ('\n') + fire + ('\n') + water + ('\n') + thunder + ('\n') + ice + ('\n') + dragon 
+                j = json.loads(values[0])
+                parts = j["monster"]["monsterbodyparts"]
+                result = "Fire | Ice | Thunder | Water | Dragon \n"
+                for i in parts:
+                    result += (i["local_name"] + " " + i["pivot"]["res_fire"] + " " + i["pivot"]["res_ice"] + " " + i["pivot"]["res_thunder"] + " " + i["pivot"]["res_water"] + " " + i["pivot"]["res_dragon"])
+                    result += "\n"
+        result = format_string(result)  
+        #print "Items"
+        #parts = j["monster"]["items"]
+        #for i in parts:
+        #    print i["local_name"] + "  Rango " + i["pivot"]["rank"]["local_name"] + " " + i["pivot"]["monsteritemmethod"]["local_name"] + " " + i["pivot"]["percentage"] + "%"
+        #result = hp + ('\n') + fire + ('\n') + water + ('\n') + thunder + ('\n') + ice + ('\n') + dragon 
     else:
         print "Status Code %d" %statusCode
         result = "No he encontrado a ese monstruo :C"
+    
     cid = m.chat.id
     bot.send_message(cid, result)
     
 @bot.message_handler(commands=['recompensa']) # Indicamos que lo siguiente va a controlar el comando '/miramacho'
 def command_recompensa(m): # Definimos una función que resuleva lo que necesitemos.
-    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
-    bot.send_message( cid, 'Aun no esta, paciencia') # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+    url = "http://kiranico.com/es/mh4u/monstruo/"
+    #print str(m.text).split(' ')[1]
+    ##Parse string
+    a = len(str(m.text).split(' '))
+    if a == 3:
+       b = str(m.text).split(' ')
+       for i in b:
+           i=i.lower()
+       del b[0]
+       print b
+       c = ('-').join(b)
+       url += c
+       req = requests.get(url)
+       statusCode = req.status_code
+    elif a == 2:
+       url += str(m.text).split(' ')[1].lower()
+       req = requests.get(url)
+       statusCode = req.status_code
+    else:
+        statusCode=500
+    #url += len(str(m.text).split(' ')[1]
+    #url += str(m.text).split(' ')[1]
+    # Realizamos la petición a la web
+    
+    
+    # Comprobamos que la petición nos devuelve un Status Code = 200
+    
+    if statusCode == 200:
+    
+        # Pasamos el contenido HTML de la web a un objeto BeautifulSoup()
+        html = BeautifulSoup(req.text)
+    
+        # Obtenemos todos los divs donde estan las entradas
+        script = html.find_all('script',{'class':''})
+    
+        # Recorremos todas las entradas para extraer el título, autor y fecha
+        fire="Resistencia fuego: "
+        water="Resistencia agua: "
+        thunder="Resistencia trueno: "
+        ice="Resistencia hielo: "
+        dragon="Resistencia dragon: "
+        for i,entradas in enumerate(script):
+            if i == 5:
+                #print entradas
+                values = re.findall(r'var.*?=\s*(.*?);', str(entradas), re.DOTALL | re.MULTILINE)
+                j = json.loads(values[0])
+                parts = j["monster"]["monsterbodyparts"]
+                result = "RANGO BAJO\n"
+                result2 = "RANGO ALTO\n"
+                result3 = "RANGO G\n"
+                parts = j["monster"]["items"]
+                firstime = True
+                jold=''
+                for i in parts:
+                    jnew = i["pivot"]["monsteritemmethod"]["local_name"]
+
+                    if (i["pivot"]["rank"]["local_name"] == "Bajo"):
+                        if (jnew != jold and not(firstime)):
+                            result += "==================================================\n"
+                        result += (i["local_name"] + " " + i["pivot"]["monsteritemmethod"]["local_name"] + " " + i["pivot"]["percentage"] + "%")
+                        result += "\n"
+
+                    elif (i["pivot"]["rank"]["local_name"] == "Alto"):
+                        if (jnew != jold and not(firstime)):
+                            result2 += "==================================================\n"
+                        result2 += (i["local_name"]+ " " + i["pivot"]["monsteritemmethod"]["local_name"] + " " + i["pivot"]["percentage"] + "%")
+                        result2 += "\n"
+                    else:
+                        if (jnew != jold and not(firstime)):
+                            result3 += "==================================================\n"
+                        result3 += (i["local_name"] + " " + i["pivot"]["monsteritemmethod"]["local_name"] + " " + i["pivot"]["percentage"] + "%")
+                        result3 += "\n"
+                    firstime=False
+                    jold = i["pivot"]["monsteritemmethod"]["local_name"]
+    else:
+        print "Status Code %d" %statusCode
+        result = "No he encontrado a ese monstruo :C"
+    cid = m.chat.id
+    bot.send_message(cid, result)
+    bot.send_message(cid, result2)
+    bot.send_message(cid, result3)
 #############################################
 #Peticiones
 bot.polling(none_stop=True) # Con esto, le decimos al bot que siga funcionando incluso si encuentra algún fallo.
